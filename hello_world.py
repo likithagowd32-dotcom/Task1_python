5dd154f6c9b153f29cb1ed6114cb83e67b80c12d
likithagowd32-dotcom <likithagowd32@gmail.com>
2026-01-15 20:02:30 +0530

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+Python Environment Setup & First Script
+
+Python Environment Setup & First Script
+
+This project demonstrates:
+- Python installation
+- Running a Python file
+- Input and output
+- Variables
+- Date usage
+
+Files:
+hello_world.py – main program
+screenshot.png – output proof
+
